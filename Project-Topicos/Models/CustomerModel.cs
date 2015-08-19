668937798d0b4b4d9423e70d4d9231d79d560516
grupo6topicos <topicos20152grupo6@gmail.com>
2015-08-19 11:10:42 -0600

    Second Update

---
+++ Project-Topicos/Models/CustomerModel.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class CustomerModel : DBCon 
+    {
+        public int CustomerID { get; set; }
+        public String FistName { get; set; }
+        public String LastName { get; set; }
+        public String PersonType { get; set; }
+        public String StoreName { get; set; }
+        public int AddressID { get; set; }
+        public String Address1 { get; set; }
+        public String Address2 { get; set; }
+        public String City { get; set; }
+        public String Province { get; set; }
+        public String Country { get; set; }
+
+
+        public DataTable ListCustomer()
+        {
+            DataTable dt_Customers = new DataTable();
+            SqlDataReader reader = null;
+            try
+            {
+                String query =  "SELECT c.[CustomerID],c.[FistName],c.[LastName],c.[PersonType],c.[StoreName],a.[AddressID],a.[Address1],a.[Address2],a.[City],a.[Province] ,a.[Country] " +
+                                "FROM [dbo_Customers].[dbo].[CustomerAddress] cs LEFT JOIN [dbo_Customers].[dbo].[Customers] c ON cs.CustomerID = c.CustomerID " +
+                                "LEFT JOIN [dbo_Customers].[dbo].[Address] a ON cs.AddressID = a.AddressID  ";
+                System.Data.SqlClient.SqlCommand sqlcmd = new System.Data.SqlClient.SqlCommand(query, this.GetObjConexion());
+                sqlcmd.CommandType = CommandType.Text;
+                this.Open();
+                reader = sqlcmd.ExecuteReader();
+                dt_Customers.Load(reader);
+                return dt_Customers;
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                reader = null;
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                    reader = null;
+                }
+                this.Close();
+            }
+        }
+
+        public DataTable ListCustomer_Country(String Country)
+        {
+            DataTable dt_Customers = new DataTable();
+            SqlDataReader reader = null;
+            try
+            {
+                String query = "SELECT c.[CustomerID],c.[FistName],c.[LastName],c.[PersonType],c.[StoreName],a.[AddressID],a.[Address1],a.[Address2],a.[City],a.[Province] ,a.[Country] " +
+                                "FROM [dbo_Customers].[dbo].[CustomerAddress] cs LEFT JOIN [dbo_Customers].[dbo].[Customers] c ON cs.CustomerID = c.CustomerID " +
+                                "LEFT JOIN [dbo_Customers].[dbo].[Address] a ON cs.AddressID = a.AddressID WHERE a.[Country] = @Country ";
+                System.Data.SqlClient.SqlCommand sqlcmd = new System.Data.SqlClient.SqlCommand(query, this.GetObjConexion());
+                sqlcmd.Parameters.AddWithValue("@Country", Country);
+                sqlcmd.CommandType = CommandType.Text;
+                this.Open();
+                reader = sqlcmd.ExecuteReader();
+                dt_Customers.Load(reader);
+                return dt_Customers;
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                reader = null;
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                    reader = null;
+                }
+                this.Close();
+            }
+        }
+
+        public DataTable ListCustomer_Province(String Province)
+        {
+            DataTable dt_Customers = new DataTable();
+            SqlDataReader reader = null;
+            try
+            {
+                String query = "SELECT c.[CustomerID],c.[FistName],c.[LastName],c.[PersonType],c.[StoreName],a.[AddressID],a.[Address1],a.[Address2],a.[City],a.[Province] ,a.[Country] " +
+                                "FROM [dbo_Customers].[dbo].[CustomerAddress] cs LEFT JOIN [dbo_Customers].[dbo].[Customers] c ON cs.CustomerID = c.CustomerID " +
+                                "LEFT JOIN [dbo_Customers].[dbo].[Address] a ON cs.AddressID = a.AddressID WHERE a.[Province] = @Province ";
+                System.Data.SqlClient.SqlCommand sqlcmd = new System.Data.SqlClient.SqlCommand(query, this.GetObjConexion());
+                sqlcmd.Parameters.AddWithValue("@Province", Province);
+                sqlcmd.CommandType = CommandType.Text;
+                this.Open();
+                reader = sqlcmd.ExecuteReader();
+                dt_Customers.Load(reader);
+                return dt_Customers;
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                reader = null;
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                    reader = null;
+                }
+                this.Close();
+            }
+        }
+
+        public DataTable ListCustomer_City(String City)
+        {
+            DataTable dt_Customers = new DataTable();
+            SqlDataReader reader = null;
+            try
+            {
+                String query = "SELECT c.[CustomerID],c.[FistName],c.[LastName],c.[PersonType],c.[StoreName],a.[AddressID],a.[Address1],a.[Address2],a.[City],a.[Province] ,a.[Country] " +
+                                "FROM [dbo_Customers].[dbo].[CustomerAddress] cs LEFT JOIN [dbo_Customers].[dbo].[Customers] c ON cs.CustomerID = c.CustomerID " +
+                                "LEFT JOIN [dbo_Customers].[dbo].[Address] a ON cs.AddressID = a.AddressID WHERE a.[City] = @City ";
+                System.Data.SqlClient.SqlCommand sqlcmd = new System.Data.SqlClient.SqlCommand(query, this.GetObjConexion());
+                sqlcmd.Parameters.AddWithValue("@City", City);
+                sqlcmd.CommandType = CommandType.Text;
+                this.Open();
+                reader = sqlcmd.ExecuteReader();
+                dt_Customers.Load(reader);
+                return dt_Customers;
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                reader = null;
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                    reader = null;
+                }
+                this.Close();
+            }
+        }
+
+        public DataTable ListCountries()
+        {
+            DataTable dt_Customers = new DataTable();
+            SqlDataReader reader = null;
+            try
+            {
+                String query =  "SELECT DISTINCT a.[Country] "+
+                                "FROM [dbo_Customers].[dbo].[CustomerAddress] cs LEFT JOIN [dbo_Customers].[dbo].[Customers] c ON cs.CustomerID = c.CustomerID "+
+                                "LEFT JOIN [dbo_Customers].[dbo].[Address] a ON cs.AddressID = a.AddressID" ;
+                System.Data.SqlClient.SqlCommand sqlcmd = new System.Data.SqlClient.SqlCommand(query, this.GetObjConexion());
+                sqlcmd.CommandType = CommandType.Text;
+                this.Open();
+                reader = sqlcmd.ExecuteReader();
+                dt_Customers.Load(reader);
+                return dt_Customers;
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                reader = null;
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                    reader = null;
+                }
+                this.Close();
+            }
+        }
+
+        public DataTable ListProvinces()
+        {
+            DataTable dt_Customers = new DataTable();
+            SqlDataReader reader = null;
+            try
+            {
+                String query = "SELECT DISTINCT a.[Province] " +
+                                "FROM [dbo_Customers].[dbo].[CustomerAddress] cs LEFT JOIN [dbo_Customers].[dbo].[Customers] c ON cs.CustomerID = c.CustomerID " +
+                                "LEFT JOIN [dbo_Customers].[dbo].[Address] a ON cs.AddressID = a.AddressID";
+                System.Data.SqlClient.SqlCommand sqlcmd = new System.Data.SqlClient.SqlCommand(query, this.GetObjConexion());
+                sqlcmd.CommandType = CommandType.Text;
+                this.Open();
+                reader = sqlcmd.ExecuteReader();
+                dt_Customers.Load(reader);
+                return dt_Customers;
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                reader = null;
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                    reader = null;
+                }
+                this.Close();
+            }
+        }
+
+        public DataTable ListCities()
+        {
+            DataTable dt_Customers = new DataTable();
+            SqlDataReader reader = null;
+            try
+            {
+                String query =  "SELECT DISTINCT a.[City] " +
+                                "FROM [dbo_Customers].[dbo].[CustomerAddress] cs LEFT JOIN [dbo_Customers].[dbo].[Customers] c ON cs.CustomerID = c.CustomerID " +
+                                "LEFT JOIN [dbo_Customers].[dbo].[Address] a ON cs.AddressID = a.AddressID";
+                System.Data.SqlClient.SqlCommand sqlcmd = new System.Data.SqlClient.SqlCommand(query, this.GetObjConexion());
+                sqlcmd.CommandType = CommandType.Text;
+                this.Open();
+                reader = sqlcmd.ExecuteReader();
+                dt_Customers.Load(reader);
+                return dt_Customers;
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                reader = null;
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                    reader = null;
+                }
+                this.Close();
+            }
+        }
+    }
+}
---
+++ Project-Topicos/Models/DBCon.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class DBCon
+    {
+        private static SqlConnection sqlConn;
+        System.Data.SqlClient.SqlConnection Conn;
+
+
+        protected DBCon()
+        {
+            Conn = new System.Data.SqlClient.SqlConnection(@"Data Source=131.190.73.250\I_TRADE;Initial Catalog=dbo_Customers;Persist Security Info=True;User ID=sa;Password=lacost");
+        }
+
+        protected System.Data.SqlClient.SqlConnection GetObjConexion()
+        {
+            return this.Conn;
+        }
+
+        public static SqlConnection getInstance()
+        {
+            if (sqlConn == null)
+            {
+                sqlConn = new SqlConnection();
+            }
+            return sqlConn;
+        }
+
+        protected Boolean Open()
+        {
+            try
+            {
+                if (Conn.State == System.Data.ConnectionState.Closed)
+                    Conn.Open();
+
+                if (Conn.State == System.Data.ConnectionState.Open)
+                    return true;
+                else
+                    return false;
+            }
+
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        protected Boolean Close()
+        {
+            try
+            {
+
+                if (Conn.State == System.Data.ConnectionState.Open)
+                    Conn.Close();
+                if (Conn.State == System.Data.ConnectionState.Closed)
+                    return true;
+                else
+                    return false;
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
